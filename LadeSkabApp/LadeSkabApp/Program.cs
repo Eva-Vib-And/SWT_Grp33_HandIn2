76cfb6063e3959a9ad25ad33d5ccae52582dcb24
Eva-Vib-And <60734490+Eva-Vib-And@users.noreply.github.com>
2021-03-20 15:36:37 +0100

    NUnit porjekt oprettet
    nuget packeges hentet

---
+++ LadeSkabApp/LadeSkabApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LadeSkabApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
